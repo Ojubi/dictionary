57b6728a77027b772211d06279c2d26b32a2d8f9
Ojubi <enoojubi202@gmail.com>
2024-12-05 00:03:55 -0800

    Add files via upload

---
+++ COS ASSIGNMENT.py
@@ -0,0 +1,15 @@
+efik_dictionary = {
+    'mbok' :  'please',
+    'ediye' : 'beautiful',
+    'afo' : 'you',
+    'nsido' : 'how are you',
+    'abasi' : 'God',
+    'akwa' : 'clothe',
+    'eka' : 'mother',
+    'ette' : 'father',
+    'uyai' : 'beauty',
+     'nkpo': 'something'
+}
+
+word = input("enter efik word: ")
+print(efik_dictionary[word])
